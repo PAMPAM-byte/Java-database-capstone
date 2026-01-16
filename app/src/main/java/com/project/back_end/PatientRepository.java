5a98641245f5afac9f649bb23eb977bee607a1dc
PAMPAM-byte <meetfavourakintade@gmail.com>
2026-01-16 10:16:41 +0100

    Remove misplaced PatientRepository

--- app/src/main/java/com/project/back_end/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.back_end.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.back_end.models.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
